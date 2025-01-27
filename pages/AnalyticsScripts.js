8f913dad2a832d09a1fe5351c84863b048e032db
Kunal Bohra <66618568+BOHRA-KUNAL@users.noreply.github.com>
2025-01-27 21:38:05 +0530

    changed google scripts

--- pages/AnalyticsScripts.js
+++ pages/AnalyticsScripts.js
@@ -4,22 +4,7 @@ const AnalyticsScripts = () => {
   return (
     <Head>
       {/* Meta Pixel Code */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-           !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '536078485927864');
-            fbq('track', 'PageView');
-          `,
-        }}
-      />
+    
       <noscript>
         <img
           height="1"
@@ -32,15 +17,15 @@ const AnalyticsScripts = () => {
       {/* Google Analytics */}
       <script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-07N7L1549H"
+        src="https://www.googletagmanager.com/gtag/js?id=G-8G2WC36RHT"
       />
       <script
         dangerouslySetInnerHTML={{
           __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-07N7L1549H');
+             window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8G2WC36RHT');
           `,
         }}
       />
--- pages/_document.js
+++ pages/_document.js
@@ -47,40 +47,64 @@ export default function Document() {
   const faqSchemaData = {
     "@context": "https://schema.org",
     "@type": "FAQPage",
-    "mainEntity": [
-      {
-        "@type": "Question",
-        "name": "What is Satta Matka?",
-        "acceptedAnswer": {
-          "@type": "Answer",
-          "text": "Satta Matka is a form of gambling that originated in India. It involves betting on numbers drawn from a pot, or 'Matka,' which is where the name comes from. Over the years, it has evolved into a sophisticated system with various types of games, each offering unique ways to place bets and win."
-        }
-      },
-      {
-        "@type": "Question",
-        "name": "How Satta Matka Works?",
-        "acceptedAnswer": {
-          "@type": "Answer",
-          "text": "In Satta Matka, players place bets on numbers drawn from a pot. The game typically involves selecting a number or a combination of numbers, and if these numbers match the drawn numbers, the player wins. The game is primarily based on luck, though some players use statistical analysis and prediction techniques to improve their chances."
-        }
-      },
-      {
-        "@type": "Question",
-        "name": "Why Choose ZARA777?",
-        "acceptedAnswer": {
-          "@type": "Answer",
-          "text": "1. User-Friendly Interface\n2. Fast and Accurate Results\n3. Trusted and Secure\n4. Exciting Bonuses and Promotions\n5. 24/7 Customer Support"
-        }
-      },
-      {
-        "@type": "Question",
-        "name": "How to Play Satta Matka on ZARA777?",
-        "acceptedAnswer": {
-          "@type": "Answer",
-          "text": "1. Visit jodiplay.com\n2. Download App\n3. Register and Create an Account\n4. Choose Your Game\n5. Place Your Bets\n6. Check Live Results\n7. Collect Your Winnings"
-        }
+    "mainEntity": [{
+      "@type": "Question",
+      "name": "What is Zara777?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Zara777 is an online platform for matka play, offering a variety of matka markets with 24/7 automated deposit and withdrawal services for a seamless gaming experience."
       }
-    ]
+    }, {
+      "@type": "Question",
+      "name": "How do I get started with matka play on Zara777?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Simply sign up on our website, fund your account through our secure payment options, and select your preferred matka market to start playing."
+      }
+    }, {
+      "@type": "Question",
+      "name": "What types of matka markets are available on Zara777?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Zara777 provides access to all popular matka markets, allowing users to choose the games they know and love."
+      }
+    }, {
+      "@type": "Question",
+      "name": "Is it safe to deposit and withdraw money on Zara777?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Yes, Zara777 prioritizes user safety with secure payment gateways and 24/7 automated deposits and withdrawals, ensuring fast and safe transactions."
+      }
+    }, {
+      "@type": "Question",
+      "name": "How fast are deposits and withdrawals processed?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "All deposits and withdrawals are processed instantly, thanks to our automated system available around the clock."
+      }
+    }, {
+      "@type": "Question",
+      "name": "Can I play matka on Zara777 at any time?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Yes, Zara777 operates 24/7, allowing you to access the platform and play whenever it’s convenient for you."
+      }
+    }, {
+      "@type": "Question",
+      "name": "What should I do if I encounter an issue with my account?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "If you have any account issues, please contact our support team through the \"Contact Us\" page. We’re here to help you resolve any problems."
+      }
+    }, {
+      "@type": "Question",
+      "name": "Is there a minimum or maximum limit for deposits and withdrawals?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Yes, Zara777 may have specific limits depending on the payment method. Please refer to our payment policy section for detailed information."
+      }
+    }]
+
   };
 
   return (
@@ -88,16 +112,16 @@ export default function Document() {
       <Head>
         <title>ZARA777 | The Best Online Satta Matka App for Fun and Wins</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Join ZARA777 for the ultimate online Satta Matka experience! Enjoy fast results, exciting games, and a friendly community. Start playing today!"  />
-        <meta name="keywords" content="ZARA777, Satta Matka, Online Matka Play, Madhur Matka, Kalyan Matka, Satta Results, Matka Guessing, Satta Matka App" />
-        <meta property="og:title" content="ZARA777: Your Ultimate Online Satta Matka Experience" />
-        <meta property="og:description" content="Discover ZARA777, your go-to online Satta Matka app! Experience thrilling games, quick results, and a vibrant community. Join now for endless fun and excitement" />
-        <meta property="og:site_name" content="Jodiplay"/>
-        <meta property="og:url" content="https://jodiplay.com"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:image" content="https://drive.google.com/uc?id=1aZZrVLYE2KOsPec4_nTdhhZ70MxBbbgZ"/>
-        <meta name="robots" content="index, follow"/>
-        <link rel="canonical" href="https://www.jodiplay.com"/>
+        <meta name="description" content="Join ZARA777 for the ultimate online Satta Matka experience! Enjoy fast results, exciting games, and a friendly community. Start playing today!" />
+        <meta name="keywords" content="online matka app, online matka play, matka app, online matka play app, online satta play, trusted online matka app, best online satta app" />
+        <meta property="og:title" content="Play Satta Matka and Win Big on ZARA777" />
+        <meta property="og:description" content="The trusted online matka app in India with fast results. Play now and enjoy quick wins and exciting games like Madhur Matka, Milan Matka, Gali Matka and more!" />
+        <meta property="og:image" content="https://zara777.in/img/zara_header_image.png" />
+        <meta property="og:url" content="https://zara777.in/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="ZARA777" />
+
+        <link rel="canonical" href="https://zara777.in/" />
 
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
